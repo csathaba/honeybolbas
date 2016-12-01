f57db9587b3c827338411448899ecb2ad6d4d61e
Csaba Thamo <csabathamo86@gmail.com>
2016-12-01 14:12:25 +0100

    Remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
-  before_filter :redirect_to_domain if Rails.env.production?
+  # before_action :redirect_to_domain if Rails.env.production?
 
   private
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def redirect_to_domain
-    domain_to_redirect_to = 'www.honeybolbas.cloud'
-    domain_exceptions = ['www.honeybolbas.cloud']
-    should_redirect = domain_exceptions.exclude? request.host
-    new_url = "#{request.protocol}#{domain_to_redirect_to}#{request.fullpath}"
-    redirect_to new_url, status: :moved_permanently if should_redirect
-  end
+  # def redirect_to_domain
+  #   domain_to_redirect_to = 'www.honeybolbas.cloud'
+  #   domain_exceptions = ['www.honeybolbas.cloud']
+  #   should_redirect = domain_exceptions.exclude? request.host
+  #   new_url = "#{request.protocol}#{domain_to_redirect_to}#{request.fullpath}"
+  #   redirect_to new_url, status: :moved_permanently if should_redirect
+  # end
 end
