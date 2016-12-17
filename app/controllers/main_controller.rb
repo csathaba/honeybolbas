390ab5ed45238c85057240ebbfc93f2de60d575b
Csaba Thamo <csabathamo86@gmail.com>
2016-12-17 12:01:46 +0100

    Work on next honey link

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,7 @@ class MainController < ApplicationController
     @honeys =  Honey.all
     @honeys_portrait =  Honey.all.where(orientation_landscape: false)
     @honeys_landscape = Honey.all.where(orientation_landscape: true)
+    # @next_honey = Honey.next
   end
 
 end
--- app/models/honey.rb
+++ app/models/honey.rb
@@ -2,4 +2,9 @@ class Honey < ApplicationRecord
   mount_uploader :honeyimg, HoneyimgUploader
 
   validates_presence_of :honeyimg, message: "Bobokám, tölts fel egy képet is!"
+
+
+  def next
+    self.class.where('id > ?', id).order(:id)
+  end
 end
