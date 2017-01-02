9bf9cefc3c81ec355cd035ddf591058ee0b26a1a
Csaba Thamo <csabathamo86@gmail.com>
2017-01-02 08:19:02 +0100

    Complete rank_up method

--- app/models/honey.rb
+++ app/models/honey.rb
@@ -8,15 +8,37 @@ class Honey < ApplicationRecord
     self.class.where('id > ?', id).order(:id)
   end
 
-   def rank_up
+  def set_ranks
     r = 1
+    self.class.order(:rank).each do |honey|
+      honey.rank = r
+      honey.save
+      r = r + 1
+    end
+  end
+
+  def rank_up
     @id = id
+    @rank = rank
+    # puts "id: #{@id}, rank: #{@rank}"
     self.class.order(:rank).each do |honey|
       if honey.id == @id
-        honey.rank = r
+        if @rank != 1
+          honey.rank = @rank - 1
+          honey.save
+        end
+      elsif honey.rank == @rank - 1
+        # puts "Time to increment the rank! id: #{honey.id}, rank: #{honey.rank}"
+        honey.rank = honey.rank + 1
         honey.save
+        # puts "After id: #{honey.id}, rank: #{honey.rank}"
       end
-      r = r+1
+    end
+  end
+
+  def print_honeys
+    self.class.order(:id).each do |honey|
+      puts "id: #{honey.id}, rank: #{honey.rank}"
     end
   end
 end
