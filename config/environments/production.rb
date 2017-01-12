6f17f92cc6db064781123920ad16e4c6f52f85db
Csaba Thamo <csabathamo86@gmail.com>
2017-01-12 09:12:45 +0100

    Configure Sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://honeybolbas.herokuapp.com' }
-
   # email enabled in production
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
